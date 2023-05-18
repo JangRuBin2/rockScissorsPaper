3de09b39e27c2c2a9076f0799335549d53e27830
jangrubin <wkdfnqls2465@gmail.com>
2023-05-18 10:58:18 +0900

    가위 냈을 때 결과 출력

--- rsp.js
+++ rsp.js
@@ -1,25 +1,16 @@
-function gambambo(na:string, you:string):string {
-  let result:string = "과연 결과는?";
-  // let rock:string = "바위"; 
-  let a, b, c;
-  a = "이겼다";
-  b = "졌다";
-  c = "비겼다";
-  // 내가 가위를 냈을 때
-  if(na ==="가위" || na ==="찌") {
-    if(you === "가위" || you === "찌") {
-      result = c;
+function gambambo(na, you) {
+    var result = true;
+    // let rock:string = "바위"; 
+    // var a, b, c;
+    // a = "rock";
+    // b = "scissors";
+    // c = "paper";
+    if (na === "가위") {
+        na = a;
+        console.log(na);
     }
-    if(you === "바위" || you === "묵") {
-      result = b;
-    }
-    if(you === "보자기" || you === "빠") {
-      result = a;
-    }
-  };
-  return result;
+    return console.log(na);
 }
-console.log(gambambo("찌","묵"))
 // ? 작업을 할 때 첫번째로 해야할 것은 조건(가위바위보)을 가지고 얽힌 생각을 풀어내야 한다.
 // ? 1. 함수가 될지 클래스가 될지 모르지만 이름을 만들어 줌
 // ? 2. 규칙을 적습니다. -> 한글로 적어야 알아 먹음(작업중이라고 써넣는다)
@@ -30,5 +21,7 @@ console.log(gambambo("찌","묵"))
 // * 보자기는 가위한테 지고, 주먹은 이긴다.
 // 함수에서 가위,바위,보 문자로 받는다면 변수선언은 필요없을듯 함
 // 그러면 문자로 받아서 하는 경우 먼저해보자
-//! a,b,c 세가지 경우의 수를 두자 "이겼다, 졌다, 비겼다"
-// ! 결과 값을 result에 담아서 return해주자
+let test= "하이";
+let a="바이";
+test = a
+console.log(test)
